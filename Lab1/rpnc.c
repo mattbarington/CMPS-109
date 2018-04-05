c31b12905ffba962544f3b7cea4e4d52ecd68d6f
Matthew Ovenden <movenden@ucsc.edu>
2018-04-04 21:54:13 -0700

    Almost working without stack struct implementation:

--- Lab1/rpnc.c
+++ Lab1/rpnc.c
@@ -13,9 +13,6 @@ typedef struct Node {
     int data;
 } Node;
 
-Node* newNode(int data) {
-
-}
 
 //Node destructor
 // void freeNode(Node* N) {
@@ -25,37 +22,49 @@ Node* newNode(int data) {
 //     }
 // }
 
+void printStack(Node* this) {
+    if (this != NULL) {
+      printf("%d, ", this->data);
+      printStack(this -> next);
+    }
+}
 
 Node* push(Node* top, int data) {
+    printf("pushing %d to the stack \n", data);
     Node* newTop = (struct Node*) malloc(sizeof(Node));
     newTop -> next = top;
     newTop -> data = data;
     return newTop;
 }
 
-int pop(Stack* stack) {
-    if (stack -> size < 1) {
+Node* pop(Node* top) {
+    if (top == NULL) {
       printf("Come on now, I ain't got no more to give.\n");
       return 0;
     }
-    printf("%d is at the top of the stack\n", stack->top->data);
-    Node* temp = stack -> top;
-    stack -> top = stack -> top -> next;
-    int result = temp -> data;
+    printf("%d is at the top of the stack\n", top->data);
+    Node* temp = top;
+    top = top -> next;
     //free previous top Node
     temp -> next = NULL;
     //free(&temp);
-    stack -> size --;
-    printf("%d is now at the top of the stack\n", stack->top->data);
-    return result;
+    printf("%d is now at the top of the stack\n", top->data);
+    return top;
 }
 
 int main(int argc, char *argv[]) {
-    Node* stack = n
+    Node* stack = NULL;
     for (int i = 2; i < 10; i++) {
-      printf("pushing %d to the stack \n", i);
-      push(stack, i);
+      stack = push(stack, i);
+      // printf("stack: ");
+      // printStack(stack);
     }
+
+    while (stack != NULL) {
+      printf("%d \n", stack->data);
+      stack = pop(stack);
+    }
+
     int ununsed; // here to fail warnings test, remove it
     printf("not implemented\n");
 
