35eeb5e68e3a24058b05138cf1d94b236cf0584d
Matthew Ovenden <movenden@ucsc.edu>
2018-05-04 22:01:58 -0700

    It's looking pretty done from here. I'm going to comment out things and do more testing

--- Lab5/src/radix.cc
+++ Lab5/src/radix.cc
@@ -7,12 +7,15 @@
 
 #include <iostream>
 #include <thread>
-#include <mutex>
+#include <unistd.h>
 
 //#include <functional>
 #include <algorithm> // for_each
 
 #include <string>
+#include <chrono>
+#include <condition_variable>
+
 
 #include "radix.h"
 
@@ -21,6 +24,18 @@ using std::thread;
 using std::string;
 using std::vector;
 //#define DB
+
+#include <ctime>
+
+
+std::string asString(const std::chrono::system_clock::time_point& tp)
+{
+// convert to system time:
+std::time_t t = std::chrono::system_clock::to_time_t(tp);
+std::string ts = std::ctime(&t);// convert to calendar time
+ts.resize(ts.size()-1); // skip trailing newline
+return ts;
+}
 
 
 RadixSort::RadixSort(const unsigned int cores) : CORES(cores) {
@@ -43,13 +58,19 @@ unsigned int RadixSort::maxLengthInt(const vector<unsigned int> &arr) {
   return max;
 }
 
-void RadixSort::printall(vector<unsigned int> & arr) {
-  for (unsigned int i : arr)
-    cout << i << std::endl;
+unsigned int RadixSort::maxLengthInt(const vector<string> &arr) {
+  int max = 0;
+  for (string str : arr) {
+    int len = str.length();
+    if (len > max) {
+      max = len;
+    }
+  }
+  return max;
 }
 
-// static void nothing() {
-//   for (int i = 3; i < 9; i++)
+// static void printall(vector<unsigned int> & arr) {
+//   for (unsigned int i : arr)
 //     cout << i << std::endl;
 // }
 
@@ -72,6 +93,32 @@ void RadixSort::msd(vector<unsigned int> &arr, unsigned int place) {
   // m.unlock();
 }
 
+int getDig(string str, int place) {
+  if ((unsigned) place < str.length()) {
+  //cout << place << "th digit of " << str << " is " << (str[place] - 48) << std::endl;
+  return str[place] - 48;
+} else
+  return 0;
+}
+
+void RadixSort::msdStrings(vector<string> &list, unsigned int place) {
+  if (maxLengthInt(list) < place) return;
+  vector<vector<string>> buckets(10);
+  for (string str : list) {
+    int dig = getDig(str, place);
+    buckets[dig].push_back(str);
+  }
+  list.clear();
+  for (vector<string> bucket : buckets) {
+    msdStrings(bucket, place + 1);
+    for (string str : bucket) {
+      list.push_back(str);
+    }
+  }
+}
+
+
+
 void RadixSort::msd(vector<unsigned int> &arr) {
 
   #ifdef DB
@@ -87,15 +134,9 @@ void RadixSort::msd(vector<unsigned int> &arr) {
   #endif
 }
 
-void joinAllThreads(vector<thread> &threads) {
-  for (thread &t : threads) {
-    t.join();
-  }
-}
 
 void RadixSort::msd(vector<std::reference_wrapper<vector<unsigned int>>> &lists) {
-
-  auto sort = [] (vector<unsigned int> &list){
+  auto sort = [&] (vector<unsigned int> &list){
     vector<string> int_to_strings;
     std::for_each( list.begin(), list.end(),
       [&int_to_strings] (unsigned int &i) {
@@ -104,16 +145,17 @@ void RadixSort::msd(vector<std::reference_wrapper<vector<unsigned int>>> &lists)
     );
     list.clear();
     std::sort(int_to_strings.begin(),int_to_strings.end());
+    // msdStrings(int_to_strings, 0);
     std::for_each( int_to_strings.begin(),  int_to_strings.end(),
       [&list] (string &s) {
-      //  unsigned int ui = (unsigned int) std::stol(s);
         list.push_back((unsigned int) std::stol(s));
       }
     );
   };
 
-  std::vector<thread> threads;
+
   int coresInUse = 0;
+  std::vector<thread> threads;
   for (std::reference_wrapper<vector<unsigned int>> list : lists) {
     if (coresInUse >= CORES) {
       for (thread &thread : threads) {
@@ -129,3 +171,59 @@ void RadixSort::msd(vector<std::reference_wrapper<vector<unsigned int>>> &lists)
     thread.join();
   }
 }
+
+//
+// std::vector<thread> threads;
+// for (std::reference_wrapper<vector<unsigned int>> list : lists) {
+//   threads.push_back(thread { [&] {
+//       // static int cores_in_use = 0;
+//       std::unique_lock<std::mutex> guard(mtx);
+//       cores_available.wait(guard, [&] {return cores_in_use < CORES;});
+//       cores_in_use ++;
+//       #ifdef DB
+//        ms = std::chrono::duration_cast<std::chrono::milliseconds >(
+//                std::chrono::system_clock::now().time_since_epoch()
+//              );
+//              std::cout << "Core start: " << cores_in_use << " " << ms.count() << std::endl;
+//       #endif
+//
+//       sort(list);
+//
+//       #ifdef DB
+//        ms = std::chrono::duration_cast<std::chrono::milliseconds >(
+//                std::chrono::system_clock::now().time_since_epoch()
+//              );
+//              std::cout << "Core end: " << cores_in_use << " " << ms.count() << std::endl;
+//       #endif
+//       cores_in_use --;
+//       cores_available.notify_all();
+//   }});
+//   threads.back().join();
+//   threads.pop_back();
+//   }
+//   for (thread& t: threads) {
+//     t.join();
+//   }
+//   #ifdef DB
+//   for (vector<unsigned int> v : lists)
+//     printall(v);
+//   #endif
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
--- Lab5/src/radix.h
+++ Lab5/src/radix.h
@@ -7,18 +7,22 @@
 
 #include <vector>
 #include <functional>
-
+#include <condition_variable>
 /*
  * Simple multi-threaded Radix Sort with support for Most Significant Digit
  * sorting only.
  */
 class RadixSort {
 private:
-    void msd(std::vector<unsigned int> &arr, unsigned int);
-    void msd(std::vector<unsigned int> &arr);
-    static unsigned int maxLengthInt(const std::vector<unsigned int> &arr);
-    static void printall(std::vector<unsigned int> & arr);
+    // static void msd(std::vector<unsigned int> &arr, unsigned int);
+    // static void msd(std::vector<unsigned int> &arr);
+    // static void msdStrings(std::vector<std::string> &, unsigned int);
+    // static unsigned int maxLengthInt(const std::vector<std::string> &arr);
+    // static unsigned int maxLengthInt(const std::vector<unsigned int> &arr);
+  //  void printall(std::vector<unsigned int> & arr);
     const int CORES;
+    // std::condition_variable cores_available;
+    // std::mutex mtx;
 public:
     /*
      * Create a multi-threaded RadiX Sort restricted to using no more than
@@ -30,5 +34,5 @@ public:
      * Perform an in-place Most Significant Digit Radix Sort on each list of
      * unsigned integers in LISTS.
      */
-    void msd(std::vector<std::reference_wrapper<std::vector<unsigned int>>> &lists);
+    void msd(std::vector<std::reference_wrapper<std::vector<unsigned int> > > &lists);
 };
